37ab65645745960a17a313dac279e689af57fadb
DarkLordikss <DarkLordikss@github.com>
2020-12-20 13:13:00 +0700

    Initial commit

---
+++ js/gal.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("a.gallery").fancybox(
+    {
+    "padding" : 5,
+    "imageScale" : true,
+    "zoomOpacity" : false,
+    "zoomSpeedIn" : 800,
+    "zoomSpeedOut" : 800,
+    "zoomSpeedChange" : 700,
+    "frameWidth" : 700,
+    "frameHeight" : 600,
+    "overlayShow" : true,
+    "overlayOpacity" : 0.9,
+    "hideOnContentClick" :false,
+    "centerOnScroll" : false
+    });
+});
